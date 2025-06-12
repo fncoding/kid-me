e604b2643d28da7bad21556e4a807b8dc67db548
fncoding <felix.coding@proton.me>
2025-06-12 13:23:26 +0200

    prod - update:node, scss, bt5

--- src/workspace/settings.py
+++ src/workspace/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / "assets"]
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
